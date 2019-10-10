37c982eeea6d4b292aef952406bb258dabdab870
root <827893449@qq.com>
2019-10-10 10:53:20 +0800

    test类修改

--- src/test/java/com/test/test.java
+++ src/test/java/com/test/test.java
@@ -1,4 +1,4 @@
-package com.yjsb;
+package com.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class YjsbApplicationTests {
+public class test {
 
     @Test
     public void contextLoads() {
